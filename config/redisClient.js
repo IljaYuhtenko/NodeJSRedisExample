f4d84d8a5b6bb7bc03599dcfef8c7d310a81a3ae
IljaYuhtenko <ilja.yuhtenko@yandex.ru>
2018-03-03 17:29:22 +0300

    Fixed some bugs

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,12 @@
 const redis = require('redis');
-const redisPadd = 'mySuperStrongRedisDatabasePass';
+const bluebird = require('bluebird');
+const redisPass = 'mySuperStrongRedisDatabasePass';
 
-var client = require.redis.createClient();
-client.auth(require.redisPass);
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+var client = redis.createClient(6789, '185.124.188.91');
+client.auth(redisPass);
 
 client.on("error", (err) => {
     console.log("Error: " + err);
--- config/requires.js
+++ config/requires.js
@@ -1,9 +1,7 @@
 module.exports = {
     koa: require('koa'),
-    redis: require('redis'),
     router: require('koa-router'), 
     logger: require('koa-logger'),
-    bluebird: require('bluebird'),
     bodyParser: require('koa-bodyparser'),
     redisPass: 'mySuperStrongRedisDatabasePass'
 };
