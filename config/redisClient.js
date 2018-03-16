0f291dbee87a690e3ed8a82a6a499fd9aa3bd533
IljaYuhtenko <ilja.yuhtenko@yandex.ru>
2018-03-16 21:57:56 +0300

    Made Postgresql integration

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const cred = require('./credentials');
 const bluebird = require('bluebird');
-const redisID = 0;
+var redisID = 0;
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
@@ -13,5 +13,13 @@ client.on("error", (err) => {
     console.log("Error: " + err);
 })
 
+/* client.on("ready", async () => {
+    let res = await pgClient.query('SELECT "redisID" FROM "public"."RedisID"');
+    redisID = res.rows[0].redisID;
+    module.exports.redisID = redisID;
+    redisID = res.rows[0].redisID;
+    console.log("Successfully synced, " + redisID);
+})
+ */
 module.exports.client = client;
 module.exports.redisID = redisID;
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,17 @@
 const client = require('../config/redisClient').client;
-const redisID = require('../config/redisClient').redisID;
 const pgClient = require('../config/postgresql').pgClient;
+var redisID = require('../config/redisClient').redisID;
+
+async function sync () {
+    let res = await pgClient.query('SELECT "redisID" FROM "public"."RedisID"');
+    redisID = res.rows[0].redisID;
+
+    res = await pgClient.query('SELECT * FROM "public"."Notes"');
+
+    for(let row of res.rows) {
+        client.hmsetAsync(row.redisID, "title", row.title, "body", row.body);
+    }
+}
 
 module.exports = {
     create: async (ctx) => {
@@ -9,17 +20,32 @@ module.exports = {
         let body = ctx.request.body.body;
         let key = redisID;
 
-        let res = await client.hmsetAsync(key.toString(), "title", title, "body", body);
+        if (key == 0) {
+            await sync();
+            key = redisID;
+        }
+ 
+        client.hmsetAsync(key.toString(), "title", title, "body", body);
+
+        pgClient.query('INSERT INTO "Notes" VALUES($1, $2, $3)', [redisID, title, body]);
 
         redisID++;
 
-        console.log(res);
+        pgClient.query('UPDATE "public"."RedisID" SET "redisID" = $1', [redisID]);
+
+        ctx.status = 200;
+
+
     },
     getAll: async (ctx) => {
-        let test = await pgClient.query('SELECT NOW() as now');
-        console.log('Pg test is - ' + test.rows[0].now);
-
+        let test = await sync();
         let keys = await client.keysAsync("*");
+
+        if (keys.length === 0) {
+            await sync();
+            keys = await client.keysAsync("*");
+        }
+
         ctx.body = "";
         for (let key of keys) {
             let title = await client.hgetAsync(key, 'title');
@@ -32,6 +58,12 @@ module.exports = {
 
         let title = await client.hgetAsync(key, 'title');
         let body = await client.hgetAsync(key, 'body');
+
+        if (title === null || body === null) {
+            await sync();
+            title = await client.hgetAsync(key, 'title');
+            body = await client.hgetAsync(key, 'body');
+        }
         
         ctx.body = title + ": " + body + "\n";
     },
@@ -40,7 +72,19 @@ module.exports = {
         let title = ctx.request.body.title;
         let body = ctx.request.body.body;
 
-        client.hmsetAsync(key, 'title', title, 'body', body);
+        let check = await client.hgetAsync(key, 'title');
+
+        if (check === null) {
+            await sync();
+            check = await client.hgetAsync(key, 'title');
+        }
+        if (check !== null) {
+            client.hmsetAsync(key, 'title', title, 'body', body);
+
+            pgClient.query('UPDATE "public"."Notes" SET "title" = $1, "body" = $2 WHERE "redisID" = $3', 
+                [title, body, key]);
+            
+        }
 
         ctx.redirect('/' + key);
     },
@@ -48,6 +92,8 @@ module.exports = {
         let key = ctx.params.id;
 
         client.hdelAsync(key, 'title', 'body');
+
+        pgClient.query('DELETE FROM "public"."Notes" WHERE "redisID" = $1', [key]);
 
         ctx.redirect('/');
     }
